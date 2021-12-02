657e9e90f44b55bb891711843d6dc6b0420430e4
Martinseeberg <91264160+Martinseeberg@users.noreply.github.com>
2021-12-02 12:50:30 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,12 @@
 #include <chrono>
 #include <thread>
 #include <assert.h>
-//#include "Class.h"
+#include "Duration.h"
 //#include "Duration.cpp"
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
-class Time
-{
-private:
-    int time;
-    int dt;
-    int alarm;
-    int set;
-    bool alarmHasBeenSet;
-
-public:
-    //constructor with default value 0
-    Time(int s = 0);
-    //setter function
-    void setTime(int s);
-    //print description of object in hh:mm:ss
-    void print();
-    //compare two time object
-    // tick for incrementing time by 1
-    bool tick();
-    bool checkAndUpdateAlarm(int dt);
-    void setalarm(int t);
-};
-
-Time ::Time(int s)
-{
-    time = s;
-}
-
-void Time ::setTime(int s)
-{
-    time = s;
-}
-bool Time::tick()
-{
-    time++; // Incrementing by 1
-
-    if (time > alarm)
-    {
-        alarmHasBeenSet = true; // If alarm is higher than time, it should be reset.
-
-        alarm = 0; // resetting to 0
-    }
-    else
-    {
-        alarmHasBeenSet = false; // Otherwise false.
-    }
-
-    return alarmHasBeenSet;
-}
-
-bool Time::checkAndUpdateAlarm(int dt)
-{
-    assert(dt > 0); // Asserting dt has to be greater than 0
-    time = time + dt;
-    if (time > alarm)
-    {
-        alarmHasBeenSet = true;
-
-        alarm = 0; // Resetting the alarm if time exceeds alarm.
-    }
-    else
-    {
-        alarmHasBeenSet = false;
-    }
-
-    return alarmHasBeenSet; // returning boolean of alarmHasBeenSet.
-}
-
-void Time::setalarm(int set)
-{
-    alarm = set; // setting alarm = set
-}
-
-void Time ::print() // Function to print time
-{
-    cout << "Time is: " << time << "s"
-         << "\n";
-}
 
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
